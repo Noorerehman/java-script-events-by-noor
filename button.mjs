ae983730a2b7101d8bae0fc8a963d8496db58ff9
humirehman <humarehman429@gmail.com>
2023-12-20 17:22:43 +0500

    events

---
+++ button.mjs
@@ -0,0 +1,13 @@
+
+
+function findsum(arr){
+    let sum=0
+    for(a=0; a<arr.lenght; a++){
+        sum +=arr[a]
+    }
+    document.write(`The array is ${arr} <br>
+    The sum of all the numbers in the array is ${sum}`)
+}
+    
+//actions can speak more louder than words
+
---
+++ script.js
@@ -0,0 +1,12 @@
+function greetings(){
+    alert('Welcome')
+}
+
+function greetings(){
+    alert('Welcome')
+}
+
+function greet_user(username){
+    document.write(`welcome to my page: ${username}`)
+}
+
